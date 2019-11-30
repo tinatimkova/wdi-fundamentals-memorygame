dc3d9cdd121bc98c8e08bce279af0222db21fb49
Kristina Timkova <tinaatimkovaa@gmail.com>
2019-11-29 23:17:46 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,21 +1,32 @@
 const cards = ["queen", "queen", "king", "king"];
 const cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[2];
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
+var checkForMatch = function(){
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
-
-if (cardsInPlay.length === 2){
 	if(cardsInPlay[0] === cardsInPlay[1]){
-	alert("You found a match!");
-	}else{
-	alert("Sorry, try again.");
+		alert("You found a match!");
+		}else{
+		alert("Sorry, try again.");
+			}
 }
-}
+
+var flipCard = function(cardId){
+	
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+
+
+	if (cardsInPlay.length === 2){
+		checkForMatch();
+		}
+	}
+
+
+flipCard(0);
+flipCard(2);
+
+
+
 
 
 
