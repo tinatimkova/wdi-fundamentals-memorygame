5804c3cc90bb8a4d437e486452e36844061dc650
Kristina Timkova <tinaatimkovaa@gmail.com>
2019-12-02 16:27:47 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,26 @@
-const cards = ["queen", "queen", "king", "king"];
+const cards = [
+	{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+	},
+	{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+	}, 
+	{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+	},
+	{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+	}
+	];
+
 const cardsInPlay = [];
 
 var checkForMatch = function(){
@@ -12,8 +34,10 @@ var checkForMatch = function(){
 
 var flipCard = function(cardId){
 	
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 
 
 	if (cardsInPlay.length === 2){
